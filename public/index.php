fd8eb22208da48db6918cb110187af32c7fc2b5b
vitaliikurhanskyi <vitalii.kurhanskyi@gmail.com>
2022-12-06 16:12:17 +0200

    05.12.2022

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Front Controller
+ * */
+
+if(PHP_MAJOR_VERSION < 8) {
+    die("You need version PHP more or equal 8.0");
+}
+
+require_once dirname(__DIR__) . "/config/init.php";
+
+?>
+
+<h1>Привет</h1>
+
+
---
+++ vendor/wfm/TSingletone.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace wfm;
+
+
+trait TSingletone
+{
+    private
+}
