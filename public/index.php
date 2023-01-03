b1f644875f331fb9310e57ef3b4c393092f5b2a8
vitaliikurhanskyi <vitalii.kurhanskyi@gmail.com>
2023-01-03 16:17:08 +0200

    03.01.2023

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
  * Front Controller
  * */
 
-echo '<p style="color:green; position:fixed; z-index:9999;">video 2-4 on 9:41 <hr></p>';
+//echo '<p style="color:green; position:fixed; z-index:9999;">video 2-4 on 9:41 <hr></p>';
 
 if(PHP_MAJOR_VERSION < 8) {
     die("You need version PHP more or equal 8.0");
