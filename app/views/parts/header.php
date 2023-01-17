7425538b9c997f3c5959497e6865fc47329772e8
vitaliykurganskiy <vitaliykurganskiy@gmail.com>
2023-01-17 18:34:22 +0200

    home 17.01.2023

--- app/views/parts/header.php
+++ app/views/parts/header.php
@@ -89,10 +89,10 @@
 
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-                  <?php new \app\widgets\menu\Menu([
-                          'class' => 'navbar-nav ms-auto mb-2 mb-lg-0',
-                          'cache' => 0,
-                  ])  ?>
+<!--                  --><?php //new \app\widgets\menu\Menu([
+//                          'class' => 'navbar-nav ms-auto mb-2 mb-lg-0',
+//                          'cache' => 0,
+//                  ])  ?>
 
 <!--                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">-->
 <!--                  <li class="nav-item">-->
