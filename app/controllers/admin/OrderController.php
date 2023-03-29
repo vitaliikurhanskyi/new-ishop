e80e32774a21388f7e010a3ceedb8cc39a383de9
vitaliykurganskiy <vitaliykurganskiy@gmail.com>
2023-03-29 19:06:58 +0300

    admin video 22 is finish

--- app/controllers/admin/OrderController.php
+++ app/controllers/admin/OrderController.php
@@ -28,4 +28,26 @@ class OrderController extends AppController
         $this->set(compact('title', 'orders', 'pagination', 'total'));
     }
 
+    public function editAction()
+    {
+        $id = get('id');
+
+        if (isset($_GET['status'])) {
+            $status = get('status');
+            if ($this->model->change_status($id, $status)) {
+                $_SESSION['success'] = 'Статус заказа изменен';
+            } else {
+                $_SESSION['errors'] = 'Ошибка изменения статуса заказа';
+            }
+        }
+
+        $order = $this->model->get_order($id);
+        if (!$order) {
+            throw new \Exception('Not found order', 404);
+        }
+        $title = "Заказ № {$id}";
+        $this->setMeta("Админка :: {$title}");
+        $this->set(compact('title', 'order'));
+    }
+
 }
